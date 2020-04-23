2a7c2a01cf8ba0dffb7064758e8f7ca899710a8d
radugrecu97 <radugrecu97@gmail.com>
2020-04-23 15:56:26 +0200

    Upload binaries to repository

---
+++ ci/variables.groovy
@@ -0,0 +1,8 @@
+//Adjust your artifactory instance name/repository and your source code repository
+env.ARTIFACTORY_NAME = "art-01"
+env.ARTIFACTORY_REPO = "conan-local"
+env.REPO_URL = 'https://github.com/radugrecu97/RC_Car.git'
+env.REPO_BRANCH = 'master'
+
+env.MAX_BUILDS = 10
+env.MAX_DAYS = 7
