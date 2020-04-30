d8e3c94462b68a739ec2eb23d5b6fd286cc89e5a
radugrecu97 <radugrecu97@gmail.com>
2020-05-01 00:09:36 +0200

    Setup build system to work for remote debugging.

--- conanfile.py
+++ conanfile.py
@@ -1,18 +1,19 @@
 from conans import ConanFile, CMake
 
 class RC_CarConan(ConanFile):
-    name = "RC_Car"
-    description = "Remote controlled Raspberry Pi Car"
+    name = "rc_car"
     version = "0.1"
     license="MIT"
+    description = "Remote controlled Raspberry Pi Car"
     #build_folder="/home/radugrecu97/CLionProjects/RC_Car/build_debug"
     #package_folder="/home/radugrecu97/CLionProjects/RC_Car/build_debug"
     settings = "os", "compiler", "build_type", "arch"
+    url = "https://github.com/radugrecu97/RC_Car.git"
     exports_sources = "CMakeLists.txt", "src/main.cpp", "LICENSE"
+    generators = "cmake"
     options = {"shared": [True, False]}
     default_options = {"shared": True}
-    url = "https://github.com/radugrecu97/RC_Car.git"
-    generators = "cmake"
+
 
     def requirements(self):
         #self.requires("wiringpi/2.46@conan/stable")
