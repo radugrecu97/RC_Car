617f7f665c4efaa517f02c60de2a015c90e436dd
radugrecu97 <radugrecu97@gmail.com>
2020-04-30 23:31:41 +0200

    Setup build system to work for remote debugging.

--- conanfile.py
+++ conanfile.py
@@ -8,8 +8,25 @@ class RC_CarConan(ConanFile):
     #build_folder="/home/radugrecu97/CLionProjects/RC_Car/build_debug"
     #package_folder="/home/radugrecu97/CLionProjects/RC_Car/build_debug"
     settings = "os", "compiler", "build_type", "arch"
-    requires = "spdlog/1.5.0", "gtest/1.10.0"
+    exports_sources = "CMakeLists.txt", "main.cpp", "LICENSE"
     options = {"shared": [True, False]}
     default_options = {"shared": True}
     url = "https://github.com/radugrecu97/RC_Car.git"
     generators = "cmake"
+
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        self.copy("rc_car", src="bin", dst="bin")
+
+    def requirements(self):
+        #self.requires("wiringpi/2.46@conan/stable")
+        self.requires("spdlog/1.5.0")
+        self.requires("gtest/1.10.0")
+
+    def deploy(self):
+        self.copy("rc_car", src="bin", dst="bin")
